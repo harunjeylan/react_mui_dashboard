1bdb069f2ee8577205988024af9f1ec19e77e223
Harun Jeylan <harunjeylanwako@gmail.com>
2022-11-14 13:52:43 +0000

    fix   'colors' is assigned a value but never used

--- src/pages/geography/index.jsx
+++ src/pages/geography/index.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import GeographyChart from "../../components/GeographyChart";
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
-import { useTheme } from "@mui/material";
-import { tokens } from "../../theme";
 const Geography = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px" height="75vh" p="2px">
       <Header title="Line CHART" subtitle="simple line chart" />
